62ce1b6f3f032fe4c8c8701f886f4ffdc8112fb1
Tomasz Dziurko <tdziurko@gmail.com>
2013-02-28 23:38:39 +0100

    Added compile function to the directive

--- app/application.js
+++ app/application.js
@@ -6,7 +6,17 @@ angular.module("navbarapp", ["controllers"])
     restrict: "E",
     replace: true,
     transclude: true,
-    templateUrl: "components/bootstrapNavbar.html"
+    templateUrl: "components/bootstrapNavbar.html",
+    compile: function(element, attrs) {
+      var li, liElements, links, index, length;
+
+      liElements = $(element).find("#navigation-tabs li");
+      for (index = 0, length = liElements.length; index < length; index++) {
+        li = liElements[index];
+        links = $(li).find("a");
+        if (links[0].textContent === attrs.currentTab) $(li).addClass("active");
+      }
+    }
   }});
 ;
 
